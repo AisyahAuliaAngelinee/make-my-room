eb3831a1853be5559e88402e0b649b7a8da823e9
アンジェリン <vincentiusclarishna@Vincentiuss-MacBook-Pro.local>
2025-02-15 21:57:36 +0700

    install drizzle orm, neon db

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
 	return (
 		<div>
 			<strong>Hello World</strong>
+			<Button>Click me</Button>
 		</div>
 	);
 }
